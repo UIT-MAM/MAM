f74511186e42cca52c4c076c5e575d7cf3ac034f
KhangPham205 <ptkhang17122005@gmail.com>
2025-12-03 13:37:04 +0700

    Update VerifySetup2FAResponseDTO.kt

--- frontend/MAM/app/src/main/java/com/example/mam/dto/authentication/VerifySetup2FAResponseDTO.kt
+++ frontend/MAM/app/src/main/java/com/example/mam/dto/authentication/VerifySetup2FAResponseDTO.kt
@@ -3,5 +3,5 @@ package com.example.mam.dto.authentication
 import com.google.gson.annotations.SerializedName
 
 data class VerifySetup2FAResponseDTO(
-    @SerializedName("recovery_codes") val recoveryCodes: List<String>?
+    @SerializedName("recoveryCodes") val recoveryCodes: List<String>?
 )
